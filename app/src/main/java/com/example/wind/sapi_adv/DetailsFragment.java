e24e268e9c5c5d2cf22ac5f1f24b1231fc6f7134
Vass Csaba <fwind4@bitbucket.org>
2018-11-22 11:50:53 +0200

    lab 6

---
+++ app/src/main/java/com/example/wind/sapi_adv/DetailsFragment.java
@@ -0,0 +1,22 @@
+package com.example.wind.sapi_adv;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailsFragment extends Fragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.details, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
--- app/src/main/java/com/example/wind/sapi_adv/MainActivity.java
+++ app/src/main/java/com/example/wind/sapi_adv/MainActivity.java
@@ -1,53 +1,23 @@
 package com.example.wind.sapi_adv;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ListingFragment listing;
+    private DataInputFragment dataInput;
+    private DetailsFragment details;
 
-    private List<Contact> contacts = new ArrayList<>();
-    private ContactAdapter adapter;
-    private Button addBut;
-    private final String TAG = "MAIN_ACTIVITY";
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addBut = findViewById(R.id.bt_add);
-        addBut.setOnClickListener(this);
 
-        ListView lvContacts = (ListView) findViewById(R.id.lv_contacts);
-        adapter = new ContactAdapter(this, contacts);
-        lvContacts.setAdapter(adapter);
-    }
+        listing = (ListingFragment) getSupportFragmentManager().findFragmentById(R.id.list);
+        dataInput = (DataInputFragment) getSupportFragmentManager().findFragmentById(R.id.data_input);
+        details = (DetailsFragment) getSupportFragmentManager().findFragmentById(R.id.details);
 
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(this, DataInputActivity.class);
-        startActivityForResult(intent, 0);
-    }
+        setContentView(R.layout.listing);
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 0) {
-            if (resultCode == 1) {
-                try {
-                    Contact c = (Contact)data.getSerializableExtra("CONT");
-                    contacts.add(c);
-                } catch (Exception e) {
-                    Log.d(TAG, "onActivityResult: conatct null");
-                }
-
-                adapter.notifyDataSetChanged();
-            }
-        }
     }
 }
